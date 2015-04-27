db39022c4e8d9711bdaf3a491ce224d6c1e501f3
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-04-27 09:51:09 +0200

    The sphere now teleports when it leaves our field. The speed boost depends on the time spent on the field, while it's limited at a factor of 25.

--- framework/T29.cpp
+++ framework/T29.cpp
@@ -1,15 +1,17 @@
 /*
- @author: Martin Jesper Low Madsen (s124320)
- @author: Mark Seidenschnur (s114740)
- @author: Christian Kjaer Laustsen (s124324)
- 
- T29: Shooting Teleporter
- 
- This floor tile registers when the sphere is upon it, and after a set time it
- teleports the sphere (changes its position coordinates) to a fixed point on 
- the playground. It also adds a speed boost to the sphere.
- 
- */
+  @author: Martin Jesper Low Madsen (s124320)
+  @author: Mark Seidenschnur (s114740)
+  @author: Christian Kjaer Laustsen (s124324)
+
+  T29: Shooting Teleporter
+
+  This floor tile registers when the sphere is upon it, energizes over
+  time and once the sphere exits the tile it teleports and multiplies
+  its velocity by an energizing factor (speedIncrease). A teleport
+  destination can be introduced in field.lvl, but falls back to (84,
+  28) if it isn't set.
+
+*/
 #include "game.h"
 #include "field.h"
 #include "T29.h"
@@ -24,30 +26,35 @@
  * @param y      the y-coordinate for the tile
  * @param pg     a two-dimensional array of pointers to fields
  */
-T29::T29(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground pg): Floor(smgr, driver, x, y, pg) {
-    // Change the texture of the tile to a different one than the default
-    texture = driver->getTexture(texturepath + "st-oxydd.png");
-    block->setMaterialTexture(0, texture);
-    // Prepare an alternate texture for when the sphere is touching the field
-    altTexture = driver->getTexture(texturepath + "st-blue-sand.png");
-    // Count the time the sphere has spent on the field. This is to create
-    // a small delay before it teleports, which makes for a nicer experience,
-    // and allows the user to notice the a texture change on the field.
-    timeSpentOnField = 0.f;
-    // The speed increase the sphere gets after being teleported
-    speedIncrease = 30.f;
+T29::T29(ISceneManager* smgr, IVideoDriver* driver, int x, int y,
+         playground pg) : Floor(smgr, driver, x, y, pg) {
+  // Change the texture of the tile to a different one than the default
+  texture = driver->getTexture(texturepath + "st-oxydd.png");
+  block->setMaterialTexture(0, texture);
+  // Prepare an alternate texture for when the sphere is touching the field
+  altTexture = driver->getTexture(texturepath + "st-blue-sand.png");
+  // Count the time the sphere has spent on the field. This is to create
+  // a small delay before it teleports, which makes for a nicer experience,
+  // and allows the user to notice the a texture change on the field.
+  timeSpentOnField = 0.f;
+  // The speed multiplication factor the sphere gets after being
+  // teleported.
+  speedIncrease = 1.f;
+
+  // Use the field.lvl to set a target teleport destination.
+  targetField = 0;
 };
 
 
 /**
- * Alter the texture of the tile to an alternate one, when a sphere 
+ * Alter the texture of the tile to an alternate one, when a sphere
  * is touching it.
  *
  * @param Sphere the sphere object that has entered the floor tile
  */
 void T29::sphereEnter(Sphere &sphere) {
-    // Change the texture
-    block->setMaterialTexture(0, altTexture);
+  // Change the texture
+  block->setMaterialTexture(0, altTexture);
 };
 
 
@@ -58,8 +65,32 @@ void T29::sphereEnter(Sphere &sphere) {
  * @param Sphere the sphere object that has entered the floor tile
  */
 void T29::sphereExit(Sphere &sphere) {
-    // Change the texture
-    block->setMaterialTexture(0, texture);
+  // Change the texture
+  block->setMaterialTexture(0, texture);
+
+  // Allow just enough time before activating, so the player sees the texture
+  // change on the field
+  if (timeSpentOnField > 0.1f) {
+    // Reset the counter
+    timeSpentOnField = 0.f;
+
+    // Set the position coordinates to an introduced field or to the
+    // bottom left if such doesn't exist.
+    vector3df position = sphere.getPosition();
+    if (targetField) {
+      position.X = targetField->getx() * fieldsize;
+      position.Z = targetField->gety() * fieldsize;
+    } else {
+      position.X = 84.0;
+      position.Z = 28.0;
+    }
+
+    sphere.setVelocity(sphere.getVelocity() * speedIncrease);
+    sphere.setPosition(position);
+  }
+
+  timeSpentOnField = 0.f;
+  speedIncrease = 0.f;
 };
 
 
@@ -73,41 +104,31 @@ void T29::sphereExit(Sphere &sphere) {
  * @param f32         the delta time
  */
 void T29::handleSphere(Sphere &sphere, position2di mousemove, f32 frameDeltaTime) {
-    timeSpentOnField += frameDeltaTime;
-    // Allow just enough time before activating, so the player sees the texture
-    // change on the field
-    if (timeSpentOnField > 0.1f) {
-        // Reset the counter
-        timeSpentOnField = 0.f;
-        // Alter the momentum, so that the sphere gets a little boost
-        vector3df velocity = sphere.getVelocity();
-        if (velocity.X < 0.f) {
-            velocity.X -= speedIncrease;
-        } else {
-            velocity.X += speedIncrease;
-        }
-        if (velocity.Z < 0.f) {
-            velocity.Z -= speedIncrease;
-        } else {
-            velocity.Z += speedIncrease;
-        }
-        // Set the position coordinates to the bottom left
-        vector3df position = sphere.getPosition();
-        position.X = 84.0;
-        position.Z = 28.0;
+  timeSpentOnField += frameDeltaTime;
 
-        sphere.setVelocity(velocity);
-        sphere.setPosition(position);
-    }
-    // Continue the progress of the sphere, using the standard method
-    sphere.standardSphereProgress(mousemove, frameDeltaTime, stdfriction);
+  // Increase the speed factor while the sphere is on the field.
+  if (speedIncrease <= 25.f) {
+    speedIncrease += 0.1f;
+  }
+
+  // Continue the progress of the sphere, using the standard method
+  sphere.standardSphereProgress(mousemove, frameDeltaTime, stdfriction);
 };
 
 
 /**
  * @return the unique field type for field T29
  */
-fieldtype T29::getFieldType(){
-    return tT29;
+fieldtype T29::getFieldType() {
+  return tT29;
 };
 
+
+/**
+ * @return the unique field type for field T29
+ */
+void T29::introduceTo(Field &f) {
+  if (!targetField) {
+    targetField = &f;
+  }
+};
--- framework/T29.h
+++ framework/T29.h
@@ -1,15 +1,15 @@
 /*
- @author: Martin Jesper Low Madsen (s124320)
- @author: Mark Seidenschnur (s114740)
- @author: Christian Kjaer Laustsen (s124324)
- 
- T29: Shooting Teleporter
- 
- This floor tile registers when the sphere is upon it, and after a set time it
- teleports the sphere (changes its position coordinates) to a fixed point on 
- the playground. It also adds a speed boost to the sphere.
- 
- */
+  @author: Martin Jesper Low Madsen (s124320)
+  @author: Mark Seidenschnur (s114740)
+  @author: Christian Kjaer Laustsen (s124324)
+
+  T29: Shooting Teleporter
+
+  This floor tile registers when the sphere is upon it, and after a set time it
+  teleports the sphere (changes its position coordinates) to a fixed point on
+  the playground. It also adds a speed boost to the sphere.
+
+*/
 #ifndef ____TT29____
 #define ____TT29____
 
@@ -17,22 +17,26 @@
 #include "field.h"
 
 
-class T29: public Floor {
-    protected:
-        // Alternate texture, for when the sphere is touching the floor field
-        ITexture *altTexture;
-        // A counter for the time the sphere has been on the field
-        f32 timeSpentOnField;
-        // Amount of velocity added to the sphere after being teleported
-        f32 speedIncrease;
-    public:
-        T29(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground pg);
-        virtual void sphereEnter(Sphere &s);
-        virtual void sphereExit(Sphere &s);
-        virtual void handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime);
-        virtual fieldtype getFieldType();
-};
+class T29 : public Floor {
+ protected:
+  // Alternate texture, for when the sphere is touching the floor field
+  ITexture *altTexture;
+  // A counter for the time the sphere has been on the field
+  f32 timeSpentOnField;
+  // Amount of velocity added to the sphere after being teleported
+  f32 speedIncrease;
+  // Teleport destination at some field.
+  Field *targetField;
+
+ public:
+  T29(ISceneManager* smgr, IVideoDriver* driver, int x, int y, playground pg);
+
+  void introduceTo(Field &f);
+  void sphereEnter(Sphere &s);
+  void sphereExit(Sphere &s);
+  void handleSphere(Sphere &s, position2di mousemove, f32 frameDeltaTime);
+  fieldtype getFieldType();
+}
 
 
 #endif
-
